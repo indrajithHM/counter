18729b58521aedf713e807209e944f482f8342a2
Indrajith H M <indrajithlikith7.com>
2023-09-30 14:47:38 +0530

    Counter App

---
+++ script.js
@@ -0,0 +1,26 @@
+const incrementbtn=document.querySelector("#increment")
+const decrementbtn=document.getElementById("decrement")
+const countertext=document.getElementById("counter")
+let counter=0;
+
+
+function increment(){
+    counter++
+    countertext.innerText=counter
+
+}
+
+function decrement(){
+    counter--
+    if(counter<0){
+        counter=0;
+    }
+    countertext.innerText=counter
+
+}
+
+incrementbtn.addEventListener('click',increment)
+decrementbtn.addEventListener("click",decrement)
+
+
+
